58c52d4093c969b0cbfa88191a0d26213f8ab14f
Dylan <dylansosa@uchicago.edu>
2023-10-10 22:49:47 -0500

    Update ageDating.sh

--- ageDating.sh
+++ ageDating.sh
@@ -6,6 +6,4 @@ cat trna_ages.tsv | sed s'/_/	/' | cut -f 1| uniq -c | sed s'/^ *// ;s/ /	/' > t
 
 cat trna_ages.tsv | cut -f 1,2 | sed s'/_[A-Z].*	/_/' | cut -f 1 > trna_ages_branches.tsv
 
-cp /home/sosa/thesis/3_tRNA_evolution/NEW_DROSOPHILID_24_DATA/ncbi_dataset/data/br8_Dmel/GCF_000001215.4.highConfidence.bed trna_ages.bed
-
 cat trna_ages_branches.tsv | sed s'/::/     /' | awk '{print $1,$1}' | sed s'/^br-*[0-9]_// ; s/ /  / ; s:^:s"\/: ; s:      :\/: ;s:$:/":' | sed s"/\"/'/g ; s/^/sed -i / ; s/$/ trna_ages.bed/" | sh
